7b9cf87f177d638040aa2c2282e784915aebe02c
Martin van Harmelen <Martin@vanharmelen.com>
2017-02-09 14:17:46 +0100

    Added playground environment

---
+++ disPLAY.py
@@ -0,0 +1,58 @@
+from RPi import GPIO
+
+GPIO.setmode(GPIO.BCM)
+red1_pin = 17
+green1_pin = 18
+blue1_pin = 22
+red2_pin = 23
+green2_pin = 24
+blue2_pin = 25
+clock_pin = 3
+a_pin = 7
+b_pin = 8
+c_pin = 9
+latch_pin = 4
+oe_pin = 2
+
+GPIO.setup(red1_pin, GPIO.OUT)
+GPIO.setup(green1_pin, GPIO.OUT)
+GPIO.setup(blue1_pin, GPIO.OUT)
+GPIO.setup(red2_pin, GPIO.OUT)
+GPIO.setup(green2_pin, GPIO.OUT)
+GPIO.setup(blue2_pin, GPIO.OUT)
+GPIO.setup(clock_pin, GPIO.OUT)
+GPIO.setup(a_pin, GPIO.OUT)
+GPIO.setup(b_pin, GPIO.OUT)
+GPIO.setup(c_pin, GPIO.OUT)
+GPIO.setup(latch_pin, GPIO.OUT)
+GPIO.setup(oe_pin, GPIO.OUT)
+
+
+def clock():
+    GPIO.output(clock_pin, 1)
+    GPIO.output(clock_pin, 0)
+
+
+def latch():
+    GPIO.output(latch_pin, 1)
+    GPIO.output(latch_pin, 0)
+
+
+def bits_from_int(x):
+    a_bit = x & 1
+    b_bit = x & 2
+    c_bit = x & 4
+    return (a_bit, b_bit, c_bit)
+
+
+def set_row(row):
+    a_bit, b_bit, c_bit = bits_from_int(row)
+    GPIO.output(a_pin, a_bit)
+    GPIO.output(b_pin, b_bit)
+    GPIO.output(c_pin, c_bit)
+
+
+def set_color(red, green, blue):
+    GPIO.output(red1_pin, red)
+    GPIO.output(green1_pin, green)
+    GPIO.output(blue1_pin, blue)
