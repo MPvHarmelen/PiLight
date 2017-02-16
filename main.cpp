beafde08b39d51ec6f1fd9835cae27abfb66be77
Michiel van der Meer <michielmeer@live.nl>
2017-02-16 14:57:59 +0100

    Run() in main

--- main.cpp
+++ main.cpp
@@ -40,28 +40,28 @@ public:
 
 // -- The following are demo image generators.
 int main(int argc, char *argv[]) {
+    std::cout << "Starting the LED controller" << '\n';
     // Init server
     LedServer serv(50007);
+    GPIO io;
+    if (!io.Init())
+        return 1;
 
-  GPIO io;
-  if (!io.Init())
-    return 1;
+    RGBMatrix m(&io);
+    RGBMatrixManipulator *updater = new DisplayUpdater(&m);
+    updater->Start(10);  // high priority
+    serv->Run();
+    // Things are set up. Just wait for <RETURN> to be pressed.
+    printf("Press <RETURN> to exit and reset LEDs\n");
+    getchar();
 
-  RGBMatrix m(&io);
-  RGBMatrixManipulator *updater = new DisplayUpdater(&m);
-  updater->Start(10);  // high priority
+    // Stopping threads and wait for them to join.
+    delete updater;
 
-  // Things are set up. Just wait for <RETURN> to be pressed.
-  printf("Press <RETURN> to exit and reset LEDs\n");
-  getchar();
+    // Final thing before exit: clear screen and update once, so that
+    // we don't have random pixels burn
+    m.ClearScreen();
+    m.UpdateScreen();
 
-  // Stopping threads and wait for them to join.
-  delete updater;
-
-  // Final thing before exit: clear screen and update once, so that
-  // we don't have random pixels burn
-  m.ClearScreen();
-  m.UpdateScreen();
-
-  return 0;
-}
+    return 0;
+    }
