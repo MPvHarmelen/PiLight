9ff4041113e10d337d791c2d97e133e99d3aa7fb
Martin van Harmelen <Martin@vanharmelen.com>
2017-02-16 18:33:16 +0100

    Added conversion struct

--- LedServer.h
+++ LedServer.h
@@ -12,7 +12,28 @@
 
 #define BUFF_SIZE 256
 
-class LedServer : public Thread{
+struct Square {
+    // x
+    // y
+    // w
+    // h
+    // colours
+    // unsigned offset:2;
+    unsigned blue:KPWMBITS;
+    unsigned green:KPWMBITS;
+    unsigned red:KPWMBITS;
+    unsigned height:4;
+    unsigned width:5;
+    unsigned y_pos:4;
+    unsigned x_pos:5;
+};
+
+union EnDecode {
+    Square square;
+    char message[4];
+};
+
+class LedServer : public Thread {
 private:
     // Sockets
     int sockfd, newsockfd;
