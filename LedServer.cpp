3412bbaea3de4a0e309f335c32a0955033be1a85
Michiel van der Meer <michielmeer@live.nl>
2017-02-16 16:48:59 +0100

    xd pointers

--- LedServer.cpp
+++ LedServer.cpp
@@ -35,7 +35,7 @@ void LedServer::Run() {
             error("ERROR reading from socket");
         std::bitset<32> bits(*buffer);
         std::cout << "Received bits: " << bits << '\n';
-        // printf("Here is the message: %u\n", (unsigned)buffer);
+        printf("Here is the message: %u\n", (unsigned)*buffer);
         n = write(newsockfd,"I got your message",18);
         if (n < 0)
             error("ERROR writing to socket");
